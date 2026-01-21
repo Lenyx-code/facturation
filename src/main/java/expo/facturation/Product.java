ba5d1bf8ba44baf1437731428069f51fc11edd6f
Lenyx-code <lenyxdev@gmail.com>
2026-01-21 21:44:37 +0100

    fail

---
+++ src/main/java/expo/facturation/Product.java
@@ -0,0 +1,16 @@
+package expo.facturation;
+
+public class Product {
+    private int id;
+    private String nom;
+    private int qte;
+
+
+    public Product(){}
+
+    public Product(int id, String nom, int qte){
+        this.id = id;
+        this.nom = nom;
+        this.qte = qte;
+    }
+}
